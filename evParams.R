e1de3b464ad8c5606eb4250c0536204e90ebd9cb
Ben Anderson <dataknut@icloud.com>
2018-10-29 11:47:00 +1300

    added Rmd includes

--- evParams.R
+++ evParams.R
@@ -7,6 +7,9 @@ evParams$repoLoc <- dkUtils::findParentDirectory("evAnalysis")
 evParams$pubLoc <- "[Centre for Sustainability](http://www.otago.ac.nz/centre-sustainability/), University of Otago: Dunedin, New Zealand"
 evParams$otagoHCS <- "the University of Otago's High-Capacity Central File Storage [HCS](https://www.otago.ac.nz/its/services/hosting/otago068353.html)"
 
+# Rmd includes
+evParams$licenseCCBY <- paste0(evParams$repoLoc, "/includes/licenseCCBY.Rmd")
+evParams$support <- paste0(evParams$repoLoc, "/includes/support.Rmd")
 # Misc
 evParams$b2Kb <- 1024 #http://whatsabyte.com/P1/byteconverter.htm
 evParams$b2Mb <- 1048576
